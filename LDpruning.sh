58fe0af7da687b4f80133daa1d4b57555ff2431d
odko2008 <46447889+odko2008@users.noreply.github.com>
2023-06-12 11:12:29 -0600

    Update LDpruning.sh

--- LDpruning.sh
+++ LDpruning.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This script is written by Jena at https://www.biostars.org/p/349015/ and solved my issue of creating a lots of dots per line in prune.out file in my previous analysis.
+# This script is written by Jena at https://www.biostars.org/p/349015/ and solved the common issue of creating a lots of dots per line in prune.out file.
 
 # SCRIPT USAGE:
 # bash plink_pruning_prep.sh filtered_snp_file.vcf
